4ff5025a7186f7b2a20d8027019ed1f51b87a65d
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2023-02-08 23:37:13 -0600

    Center social icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
 					<a className="social-icon" href="https://github.com/PeterBohai" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faGithub} /></a>
 					<a className="social-icon" href="https://linkedin.com/in/peterbohai/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
 					<a className="social-icon" href="https://www.youtube.com/@PeterBohai" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} /></a>
-					<a className="social-icon" href="https://twitter.com/PeterBohai" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+					<a className="social-icon" style={{marginRight: 0}} href="https://twitter.com/PeterBohai" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
 				</div>
 			</div>
 			<div className="copyright">
